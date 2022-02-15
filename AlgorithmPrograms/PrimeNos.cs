bdcdba16269bef5f295aa739eec8479019c72c44
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-16 02:02:03 +0530

    [Nikhil Kumar]:PrimeNo in Range Program Added

--- AlgorithmPrograms/PrimeNos.cs
+++ AlgorithmPrograms/PrimeNos.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace AlgorithmPrograms
 {
-    internal class PrimeNos
+    public class PrimeNos
     {
+        int end = 1000, count = 0;
+        public void FindPrime()
+        {
+            for (int i = 1; i < end; i++)
+            {
+                for (int j = 1; j < end; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.Write(i + " ");
+                }
+                count = 0;
+            }
+        }
     }
 }
